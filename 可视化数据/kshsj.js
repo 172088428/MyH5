c5acad0ecbf7c40174edc186228c9f0ed9521ad2
172088428 <71575719+172088428@users.noreply.github.com>
2021-02-24 11:08:00 +0800

    Set theme jekyll-theme-cayman

--- 可视化数据/kshsj.js
+++ 可视化数据/kshsj.js
@@ -1,19 +1,20 @@
-var t=null;
-t=setTimeout(time,1000);
-function time (){
-    clearTimeout(t);
-    dt=new Date();
-    var y = dt.getFullYear();
-    var mt =dt.getUTCMonth()+1;
-    var day =dt.getDate();
-    var wks ="星期" + "日一二三四五六".charAt(dt.getDay());
-    var h=dt.getHours();
-    var m=dt.getMinutes();
-    var s=dt.getSeconds();
-    document.querySelector(".showtime").innerHTML=y+"年"+mt+"月"+day+" "+h+"时"+m+"分"+s+"秒"+" "+wks;
-    t=setTimeout(time,1000);
+var t = null;
+t = setTimeout(time, 1000);
+
+function time() {
+	clearTimeout(t);
+	dt = new Date();
+	var y = dt.getFullYear();
+	var mt = dt.getUTCMonth() + 1;
+	var day = dt.getDate();
+	var wks = "星期" + "日一二三四五六".charAt(dt.getDay());
+	var h = dt.getHours();
+	h = h < 10 ? '0' + h : h
+	var m = dt.getMinutes();
+	m = m < 10 ? '0' + m : m
+	var s = dt.getSeconds();
+	s = s < 10 ? '0' + s : s
+	document.querySelector(".showtime").innerHTML = y + "年" + mt + "月" + day + " " + h + "时" + m + "分" + s + "秒" + " " +
+		wks;
+	t = setTimeout(time, 1000);
 }
-
-
-
-
